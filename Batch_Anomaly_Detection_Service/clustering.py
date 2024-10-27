e559d0787a7a16ac72c3a5f8fff96f11cc708847
ShedrachIkenna <Shedrach686@gmail.com>
2024-10-27 19:50:31 +0100

    Added simulated ride speeds and ride distances in clustering.py

--- Batch_Anomaly_Detection_Service/clustering.py
+++ Batch_Anomaly_Detection_Service/clustering.py
@@ -9,3 +9,28 @@ from numpy.random import MT19937 # MT19937 (Mersenne Twister algorithm) is a ran
 # SeedSequence ensures reproducibility 
 from numpy.random import RandomState, SeedSequence # RandomState provides a way to set a state so that you can reproduce the same sequence of random numbers every time you run the code.
 rs = RandomState(MT19937(SeedSequence(123456789))) # rs can now be used to generate random numbers with controlled randomness 
+
+
+def simulate_ride_distances(): # This function simulates ride distances 
+    logging.info('Simulating ride distances...')
+    ride_dists = np.concatenate(
+        (
+            10 * np.random.random(size=370),
+            30 * np.random.random(size=10),
+            10 * np.random.random(size=10),
+            10 * np.random.random(size=10),
+        )
+    )
+    return ride_dists
+
+def simulate_ride_speeds():
+    logging.info('Simulating ride speeds...')
+    ride_speeds = np.concatenate(
+        (
+            np.random.normal(loc=30, scale=5, size=370),
+            np.random.normal(loc=30, scale=5, size=10),  # same speed
+            np.random.normal(loc=50, scale=10, size=10),  # high speed
+            np.random.normal(loc=15, scale=4, size=10)  # low speed
+        )
+    )
+    return ride_speeds
